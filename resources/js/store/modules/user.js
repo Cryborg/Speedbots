ee322ee05d251c299d354f0da94a392aea9d58de
Romain Seite <seite.romain@yahoo.com>
2021-01-10 20:05:30 +0100

    Init VueJS + Login page with first store

---
+++ resources/js/store/modules/user.js
@@ -0,0 +1,37 @@
+// initial state
+const state = () => ({
+  token: '',
+})
+
+// getters
+const getters = {
+
+}
+
+// actions
+const actions = {
+    login( { commit }, data ){
+        axios.post('/api/login', data)
+        .then(function (response) {
+            commit("setToken", response.data.token);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
+}
+
+// mutations
+const mutations = {
+  setToken (state, token) {
+    state.token = token
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
