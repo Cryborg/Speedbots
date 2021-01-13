5eba66944457cd5053ae6f62040dfd28fda7d5ed
Romain Seite <seite.romain@yahoo.com>
2021-01-13 23:57:20 +0100

    Add Persistence for user store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,11 +2,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import VuexPersistence from 'vuex-persist'
+
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  reducer: (state) => ({ user: state.user }), //only save navigation module
+})
+ 
 
 export default new Vuex.Store({
   modules: {
     user,
   },
+  plugins: [vuexLocal.plugin]
 })
